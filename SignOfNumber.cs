aaf84f88ddef9b88d281d7a5dcf9b0fc685356d2
SmileyVi <SmileyVi@users.noreply.github.com>
2017-07-04 22:45:26 +0300

    Add files via upload

---
+++ SignOfNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02SignOfNumber
+{
+    class SignOfNumber
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            CheckValue(input);
+        }
+        static void CheckValue( int a)
+        {
+            if (a<0)
+            { 
+                Console.WriteLine("The number " + a + " is negative.");
+            }
+            else if (a==0)
+            {
+                Console.WriteLine("The number " + a + " is zero.");
+            }
+            else 
+            {
+                Console.WriteLine("The number " + a + " is positive.");
+            }
+        }
+    }
+}
